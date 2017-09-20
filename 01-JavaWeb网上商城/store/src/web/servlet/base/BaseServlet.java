5f9ec93adcf1b475a5539f8a564f131c11b6b46c
luguanxing <651571925@qq.com>
2017-09-21 00:52:46 +0800

    抽取通用servlet

--- 01-JavaWeb网上商城/store/src/web/servlet/base/BaseServlet.java
+++ 01-JavaWeb网上商城/store/src/web/servlet/base/BaseServlet.java
@@ -16,13 +16,13 @@ public class BaseServlet extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
-			//1.获取方法名称(处理没有method参数默认为index)
+			//1.获取方法名称(注意在没有method参数时默认设其为index)
 			String methodName =  request.getParameter("method");
-			if (methodName == null || methodName.trim().isEmpty()) {
+			if (methodName == null) {
 				methodName = "index";
 			}
 
-			//2.通过反射获取BaseServlet方法对象(处理method参数不存在默认为index)
+			//2.通过反射获取BaseServlet方法对象(注意在找不到method对应方法时转到index)
 			Method method;
 			try {
 				method = this.getClass().getMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
