3946b3120d592986ba185377fa7e3da9d4483013
luguanxing <651571925@qq.com>
2017-10-05 00:43:34 +0800

    数据库结构

---
+++ 01-JavaWeb网上商城/数据库结构.sql
@@ -0,0 +1,103 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.7.3-m13 : Database - db_store
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_store` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `db_store`;
+
+/*Table structure for table `orderitem` */
+
+DROP TABLE IF EXISTS `orderitem`;
+
+CREATE TABLE `orderitem` (
+  `itemid` varchar(32) NOT NULL,
+  `count` int(11) DEFAULT NULL,
+  `subtotal` double DEFAULT NULL,
+  `pid` varchar(32) DEFAULT NULL,
+  `oid` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`itemid`),
+  KEY `fk_0001` (`pid`),
+  KEY `fk_0002` (`oid`),
+  CONSTRAINT `fk_0001` FOREIGN KEY (`pid`) REFERENCES `t_product` (`pid`),
+  CONSTRAINT `fk_0002` FOREIGN KEY (`oid`) REFERENCES `t_orders` (`oid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `t_category` */
+
+DROP TABLE IF EXISTS `t_category`;
+
+CREATE TABLE `t_category` (
+  `cid` varchar(32) NOT NULL,
+  `cname` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`cid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `t_orders` */
+
+DROP TABLE IF EXISTS `t_orders`;
+
+CREATE TABLE `t_orders` (
+  `oid` varchar(32) NOT NULL,
+  `ordertime` datetime DEFAULT NULL,
+  `total` double DEFAULT NULL,
+  `state` int(11) DEFAULT NULL,
+  `address` varchar(30) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `telephone` varchar(20) DEFAULT NULL,
+  `uid` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`oid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `t_product` */
+
+DROP TABLE IF EXISTS `t_product`;
+
+CREATE TABLE `t_product` (
+  `pid` varchar(32) NOT NULL,
+  `pname` varchar(50) DEFAULT NULL,
+  `market_price` double DEFAULT NULL,
+  `shop_price` double DEFAULT NULL,
+  `pimage` varchar(200) DEFAULT NULL,
+  `pdate` date DEFAULT NULL,
+  `is_hot` int(11) DEFAULT NULL,
+  `pdesc` varchar(255) DEFAULT NULL,
+  `pflag` int(11) DEFAULT NULL,
+  `cid` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`pid`),
+  KEY `sfk_0001` (`cid`),
+  CONSTRAINT `sfk_0001` FOREIGN KEY (`cid`) REFERENCES `t_category` (`cid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `t_user` */
+
+DROP TABLE IF EXISTS `t_user`;
+
+CREATE TABLE `t_user` (
+  `uid` varchar(32) NOT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `telephone` varchar(20) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `sex` varchar(10) DEFAULT NULL,
+  `state` int(11) DEFAULT NULL,
+  `code` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
