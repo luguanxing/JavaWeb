f4e6bc684bee63f356f9862bdf74bb24b75f6c7f
luguanxing <651571925@qq.com>
2018-02-14 00:39:35 +0800

    SSM个人博客

---
+++ 05-SSM个人博客/数据库.sql
@@ -0,0 +1,91 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.7.3-m13 : Database - db_blog
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_blog` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `db_blog`;
+
+/*Table structure for table `blog` */
+
+DROP TABLE IF EXISTS `blog`;
+
+CREATE TABLE `blog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) DEFAULT NULL,
+  `image` varchar(300) DEFAULT NULL,
+  `summary` varchar(400) DEFAULT NULL,
+  `releaseDate` datetime DEFAULT NULL,
+  `priority` int(11) NOT NULL DEFAULT '0',
+  `content` text,
+  `typeId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `typeId` (`typeId`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `blogger` */
+
+DROP TABLE IF EXISTS `blogger`;
+
+CREATE TABLE `blogger` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `profile` text,
+  `nickname` varchar(50) DEFAULT NULL,
+  `sign` varchar(100) DEFAULT NULL,
+  `image` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `blogtype` */
+
+DROP TABLE IF EXISTS `blogtype`;
+
+CREATE TABLE `blogtype` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `typeName` varchar(30) DEFAULT NULL,
+  `orderNo` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `comment` */
+
+DROP TABLE IF EXISTS `comment`;
+
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userip` varchar(128) DEFAULT NULL,
+  `nickname` varchar(50) DEFAULT NULL,
+  `content` varchar(1000) DEFAULT NULL,
+  `commentDate` datetime DEFAULT NULL,
+  `state` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `link` */
+
+DROP TABLE IF EXISTS `link`;
+
+CREATE TABLE `link` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `linkName` varchar(100) DEFAULT NULL,
+  `linkUrl` varchar(200) DEFAULT NULL,
+  `orderNo` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
